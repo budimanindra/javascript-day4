d47c550504da3bb0fbc079d249a6d2f1bc673bc5
Indra Budiman <budimanindra98@gmail.com>
2020-12-28 23:04:32 +0700

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,14 @@
+function polindromChecker(str) {  
+    var word = str.length;
+    for (var i = 0; i<word / 2; i++) {
+        if (str.charAt(i) !== str.charAt(word - 1 - i)) {
+            return false;
+        }
+    }return true;
+}
+
+if (polindromChecker("abba")) {
+    console.log("Palindrom");
+} else {
+    console.log("Bukan Palindrom");
+}
